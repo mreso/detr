115ab560a10854f384a5ad9df10bdd281c46fa09
Matthias Reso <13337103+mreso@users.noreply.github.com>
2022-07-09 04:09:57 +0000

    Fix datapipe version in distributed environments

--- main.py
+++ main.py
@@ -200,7 +200,7 @@ def main(args):
     print("Start training")
     start_time = time.time()
     for epoch in range(args.start_epoch, args.epochs):
-        if args.distributed:
+        if args.distributed and 'sampler_train' in locals():
             sampler_train.set_epoch(epoch)
         train_stats = train_one_epoch(
             model, criterion, data_loader_train, optimizer, device, epoch,
